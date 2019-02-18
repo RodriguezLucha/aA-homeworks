59d5b273180f7297a6cfb1b57ce53232b1d5a4e5
Rudy <RodriguezLucha@gmail.com>
2019-02-17 20:31:53 -0800

    Add code for job listing

--- W7D1/job_listing/frontend/components/widget.jsx
+++ W7D1/job_listing/frontend/components/widget.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import Job from './job';
 import selectLocation from './../actions';
+import Job from './job';
 
 class Widget extends React.Component {
-
   constructor(props) {
     super(props);
     this.forceUpdate = this.forceUpdate.bind(this);
 
     // require this component to re-render whenever the store's state changes
     this.props.store.subscribe(this.forceUpdate);
-    this.cities = ["San Francisco", "Seattle", "New York", "Austin", "Remote"];
+    this.cities = ['San Francisco', 'Seattle', 'New York', 'Austin', 'Remote'];
     this.selectLocation = selectLocation.bind(this);
   }
 
@@ -18,40 +17,38 @@ class Widget extends React.Component {
     $.ajax({
       crossDomain: true,
       dataType: 'jsonp',
+      type: 'GET',
       url: `https://jobs.github.com/positions.json?location=${city}&markdown=true`,
-      type: "GET",
       success: function(resp) {
         // tell the store to update with the new location and jobs;
         // use the action creator 'selectLocation' to build the object to
         // be dispatched
-        this.props.store.dispatch(this.selectLocation(city, resp))
-
+        this.props.store.dispatch(this.selectLocation(city, resp));
       }.bind(this)
     });
   }
 
   render() {
-
     // get the store's current state and deconstruct it into 'jobs'
     // and 'city' variables
-    const { city, jobs } = this.props.store.getState();
+    const {city, jobs} = this.props.store.getState();
     const cityOptions = this.cities.map(city => (
-        <button onClick={ () => { this.fetchJobListings(city) }}
-             key={city}
-             className="job-option">
-          {city}
-        </button>
-      )
+      <button className="job-option"
+        key={city}
+        onClick={ () => { this.fetchJobListings(city); }}>
+        {city}
+      </button>
+    )
     );
 
     const jobListings = jobs.map(job => (
-      <Job key={job.id}
-            title={job.title}
-            company={job.company}
-            location={job.location}
-            type={job.type}
-            description={job.description}
-            info = {job.url}/>
+      <Job company={job.company}
+        description={job.description}
+        info = {job.url}
+        key={job.id}
+        location={job.location}
+        title={job.title}
+        type={job.type}/>
     )
     );
 
@@ -64,15 +61,15 @@ class Widget extends React.Component {
           Location:
           {cityOptions}
         </div>
-        
+
         <h3>{jobListings.length} Job Listings</h3>
         <ol className="listings-list">
-            {jobListings}
+          {jobListings}
         </ol>
       </div>
     );
   }
-};
+}
 
 
 export default Widget;
--- W7D1/job_listing/frontend/reducer.js
+++ W7D1/job_listing/frontend/reducer.js
@@ -1,10 +1,18 @@
 const initialState = {
-  city: "Please Select", 
+  city: 'Please Select',
   jobs: []
 };
 
 const reducer = (state = initialState, action) => {
-  return state; // remove this and fill out the body of the reducer function
+  switch(action.type){
+  case 'SWITCH_LOCATION':
+    return {
+      ...state,
+      city: action.city
+    };
+  default:
+    return state;
+  }
 };
 
 export default reducer;
