b2c8b77cf6067fe1db60606358d0b1cc5f624af7
Rudy <RodriguezLucha@gmail.com>
2019-02-20 21:14:19 -0800

    Finish Phase 2 adding links to different colors

--- W7D4/components/rainbow.jsx
+++ W7D4/components/rainbow.jsx
@@ -9,16 +9,28 @@ import Red from './red';
 import Green from './green';
 import Blue from './blue';
 import Violet from './violet';
+import Orange from './orange';
+import Yellow from './yellow';
+import Indego from './indigo';
 
 class Rainbow extends React.Component {
   render() {
     return (
       <div>
         <h1>Rainbow Router!</h1>
-        {/* Your links should go here */}
+        <Link to='/red' >Red</Link>
+        <Link to='/green' >Green</Link>
+        <Link to='/blue' >Blue</Link>
+        <Link to='/blue/indigo' >Add indigo</Link>
 
         <div id="rainbow">
-          {/* Your routes should go here */}
+          <Route path="/red" component={Red} />
+          <Route path="/red/orange" component={Orange} />
+          <Route path="/red/yellow" component={Yellow} />
+          <Route path="/green" component={Green} />
+          <Route path="/blue" component={Blue} />
+          <Route path="/blue/indigo" component={Indego} />
+          <Route path="/violet" component={Violet} />
         </div>
       </div>
     );
